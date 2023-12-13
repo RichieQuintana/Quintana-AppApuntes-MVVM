9f074fb1da7659a9c43ad530b50c31930141295d
Ricardo Quintana <ricardo.quintana.maldonado@udla.edu.ec>
2023-12-13 08:41:01 -0500

    Creamos el modelo Vista, en el panel Explorador de soluciones de Visual Studio, haga doble clic en ViewModels\NoteViewModel.cs.

--- Views/AboutPage.xaml.cs
+++ Views/AboutPage.xaml.cs
@@ -2,11 +2,11 @@ namespace Quintana_AppApuntes.Views;
 
 public partial class AboutPage : ContentPage
 {
-	public AboutPage()
-	{
-		InitializeComponent();
-	}
-    private async void LearnMore_Clicked(object sender, EventArgs e)
+    public AboutPage()
+    {
+        InitializeComponent();
+    }
+private async void LearnMore_Clicked(object sender, EventArgs e)
     {
         if (BindingContext is Models.About about)
         {
